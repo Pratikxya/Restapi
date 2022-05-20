7d970ab36e25316a4c01c525d4240e06e8f0ac7c
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-05-20 16:30:58 +0545

    .env file added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express"; //imports the package express
 import mongoose from "mongoose"; //imports the package mongoose
+import "dotenv/config"; //imports the package dotenv
 
 const app = express(); //executes the express function
 
@@ -13,7 +14,6 @@ const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //CONNECT TO DB
-mongoose.connect(
-  "mongodb+srv://Prati:<x09isLkkt0GowMZ1>@cluster0.n0zcg.mongodb.net/test",
-  () => console.log("Connected to DB")
+mongoose.connect(process.env.DB_CONNECTION, () =>
+  console.log("Connected to DB")
 );
