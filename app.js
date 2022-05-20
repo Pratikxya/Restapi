d384610450e4eaf88c948c7fb47d1a08fe11a0e7
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-05-20 16:26:30 +0545

    Connected to DB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express"; //imports the package express
+import mongoose from "mongoose"; //imports the package mongoose
 
 const app = express(); //executes the express function
 
@@ -10,3 +11,9 @@ app.get("/posts", (req, res) => res.send("Hello World!"));
 const port = process.env.port || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+//CONNECT TO DB
+mongoose.connect(
+  "mongodb+srv://Prati:<x09isLkkt0GowMZ1>@cluster0.n0zcg.mongodb.net/test",
+  () => console.log("Connected to DB")
+);
